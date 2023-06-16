6d1a59a09888ab7777eac972ec5c5f725514d6cb
Ruiqi Zhu <zhuruiqi1@gmail.com>
2023-06-16 15:25:23 +0100

    initial test version

---
+++ cross_morphology/effect_cycle_transfer/alignexp.py
@@ -0,0 +1,161 @@
+import os
+
+import gym
+import torch
+import random
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+from options import get_options
+from cycle.data import CycleData
+from cycle.dyncycle import CycleGANModel
+from cycle.utils import init_logs
+from termcolor import cprint
+
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+
+setup_seed(0)
+
+
+def add_errors(model, display):
+    errors = model.get_current_errors()
+    for key, value in errors.items():
+        display += '{}:{:.4f}  '.format(key, value)
+    return display
+
+
+def train(args):
+    txt_logs, img_logs, weight_logs = init_logs(args)
+    data_agent = CycleData(args)  # normalize and initial the pre-collected source and target domain data
+    model = CycleGANModel(args)  # initialize all the needed networks
+    model.iengine.train_statef(data_agent.data2)  # train the target inverse dynamics
+
+    cprint('evaluate the initial transfered policy in the target domain', 'blue')
+    model.cross_policy.eval_policy(
+        gxmodel=model.netG_2to1,
+        axmodel=model.net_action_G_1to2,
+        eval_episodes=1)
+
+    best_reward = 0
+
+    for iteration in range(3):
+        cprint('###### iteration {} #######'.format(iteration + 1), 'red', 'on_blue')
+
+        args.lr_Gx = 1e-4
+        args.lr_Ax = 0
+        model.update(args)
+        end_id = 0
+        start_id = end_id
+        end_id = start_id + args.pair_n
+        cprint('update G phase', 'blue')
+        for batch_id in range(start_id, end_id):
+            item = data_agent.sample()
+            data1, data2 = item
+            model.set_input(item)
+            model.optimize_parameters()
+            # real, fake = model.fetch()
+
+            if (batch_id + 1) % args.display_gap == 0:
+                display = '\n===> Batch[{}/{}]'.format(batch_id + 1, args.pair_n)
+                print(display)
+                display = add_errors(model, display)
+                txt_logs.write('{}\n'.format(display))
+                txt_logs.flush()
+
+                path = os.path.join(img_logs, 'imgA_{}.jpg'.format(batch_id + 1))
+                model.visual(path)
+
+            if (batch_id + 1) % args.eval_gap == 0:
+                reward = model.cross_policy.eval_policy(
+                    gxmodel=model.netG_2to1,
+                    axmodel=model.net_action_G_1to2,
+                    eval_episodes=args.eval_n)
+                if reward > best_reward:
+                    best_reward = reward
+                    model.save(weight_logs)
+                print('best_reward:{:.1f}  cur_reward:{:.1f}'.format(best_reward, reward))
+
+        args.init_start = False
+        args.lr_Gx = 0
+        args.lr_Ax = 1e-4
+        model.update(args)
+        end_id = 0
+        start_id = end_id
+        end_id = start_id + args.pair_n
+        cprint('update A phase', 'blue')
+        for batch_id in range(start_id, end_id):
+            item = data_agent.sample()
+            data1, data2 = item
+            model.set_input(item)
+            model.optimize_parameters()
+            # real, fake = model.fetch()
+
+            if (batch_id + 1) % args.display_gap == 0:
+                display = '\n===> Batch[{}/{}]'.format(batch_id + 1, args.pair_n)
+                print(display)
+                display = add_errors(model, display)
+                txt_logs.write('{}\n'.format(display))
+                txt_logs.flush()
+
+                path = os.path.join(img_logs, 'imgA_{}.jpg'.format(batch_id + 1))
+                model.visual(path)
+
+            if (batch_id + 1) % args.eval_gap == 0:
+                reward = model.cross_policy.eval_policy(
+                    gxmodel=model.netG_2to1,
+                    axmodel=model.net_action_G_1to2,
+                    eval_episodes=args.eval_n)
+                if reward > best_reward:
+                    best_reward = reward
+                    model.save(weight_logs)
+
+                print('best_reward:{:.1f}  cur_reward:{:.1f}'.format(best_reward, reward))
+
+
+# def test(args):
+#     args.istrain = False
+#     args.init_start = False
+#     txt_logs, img_logs, weight_logs = init_logs(args)
+#     data_agent = CycleData(args)
+#     model = CycleGANModel(args)
+#     model.fengine.train_statef(data_agent.data1)
+#     print(weight_logs)
+#     model.load(weight_logs)
+#     model.update(args)
+#
+#     model.cross_policy.eval_policy(
+#         gxmodel=model.netG_B,
+#         axmodel=model.net_action_G_A,
+#         # imgpath=img_logs,
+#         eval_episodes=10)
+
+
+if __name__ == '__main__':
+    args = get_options()
+
+    # source env information
+    env_name = args.env
+    env = gym.make(env_name)
+    args.state_dim1 = env.observation_space.shape[0]
+    args.action_dim1 = env.action_space.shape[0]
+    env.close()
+
+    # target env information
+    env_name = args.target_env
+    env = gym.make(env_name)
+    args.state_dim2 = env.observation_space.shape[0]
+    args.action_dim2 = env.action_space.shape[0]
+    env.close()
+
+    train(args)
+    # args.istrain = False
+    # with torch.no_grad():
+    #     test(args)
---
+++ cross_morphology/effect_cycle_transfer/cycle/dyncycle.py
@@ -0,0 +1,309 @@
+import os
+import torch
+from tqdm import tqdm
+import numpy as np
+from collections import OrderedDict
+from torch.autograd import Variable
+from torchvision import transforms
+import matplotlib.pyplot as plt
+import itertools
+
+from .models import S2S, SDmodel, AGmodel, ADmodel, Fengine, Iengine
+from .utils import ImagePool, GANLoss
+from .crosspolicy import CrossPolicy
+
+
+class CycleGANModel():
+    def __init__(self, opt):
+        self.opt = opt
+        self.isTrain = opt.istrain
+        self.Tensor = torch.cuda.FloatTensor
+
+        # 1,A -> source, 2,B -> target
+
+        # load the pre-trained policy in the source domain, while the eval env is the target env
+        self.cross_policy = CrossPolicy(opt)
+        # state mapping: target -> source
+        self.netG_2to1 = S2S(opt, dir='2to1').cuda()
+        # state mapping: source -> target
+        self.netG_1to2 = S2S(opt, dir='1to2').cuda()
+        # action mapping: source -> target
+        self.net_action_G_1to2 = AGmodel(opt, '1to2').cuda()  # map: source state X action -> target action
+
+        # target inverse dynamics model
+        self.iengine = Iengine(opt)  # y_t x y_t+1 -> u_t
+        self.netI_2 = self.iengine.imodel.cuda()
+
+        self.reset_buffer()
+        # source state discriminator
+        self.netD_1 = SDmodel(opt, dir='2to1').cuda()
+        self.netD_2 = SDmodel(opt, dir='1to2').cuda()
+
+        self.fake_A_pool = ImagePool(pool_size=128)
+        self.fake_B_pool = ImagePool(pool_size=128)
+        self.fake_action_A_pool = ImagePool(pool_size=128)
+        self.fake_action_B_pool = ImagePool(pool_size=128)
+        # define loss functions
+        self.criterionGAN = GANLoss(tensor=self.Tensor).cuda()
+        if opt.loss == 'l1':
+            self.criterionCycle = torch.nn.L1Loss()
+            self.criterionIdt = torch.nn.L1Loss()
+        elif opt.loss == 'l2':
+            self.criterionCycle = torch.nn.MSELoss()
+            self.criterionIdt = torch.nn.MSELoss()
+        # initialize optimizers
+        self.optimizer_G = torch.optim.Adam([{'params': self.netI_2.parameters(), 'lr': 0.0},
+                                             {'params': self.net_action_G_1to2.parameters(), 'lr': opt.lr_Ax},
+                                             {'params': self.netG_1to2.parameters(), 'lr': opt.lr_Gx},
+                                             {'params': self.netG_2to1.parameters(), 'lr': opt.lr_Gx}])
+        self.optimizer_D_1 = torch.optim.Adam(self.netD_1.parameters())
+        self.optimizer_D_2 = torch.optim.Adam(self.netD_2.parameters())
+
+        self.init_start = True
+
+        print('-----------------------------------------------')
+        print('---------- Networks initialized ---------------')
+        print('-----------------------------------------------')
+
+    def update(self, opt):
+        self.init_start = opt.init_start
+        self.net_action_G_1to2.init_start = opt.init_start
+        self.optimizer_G = torch.optim.Adam([{'params': self.netI_2.parameters(), 'lr': 0.0},
+                                             {'params': self.net_action_G_1to2.parameters(), 'lr': opt.lr_Ax},
+                                             {'params': self.netG_1to2.parameters(), 'lr': opt.lr_Gx},
+                                             {'params': self.netG_2to1.parameters(), 'lr': opt.lr_Gx}])
+        print('\n-----------------------------------------------')
+        print('------------ model phase updated! -------------')
+        print('-----------------------------------------------\n')
+
+    def set_input(self, input):
+        data1, data2 = input
+        self.input_At0 = self.Tensor(data1[0])
+        self.action_A = self.Tensor(data1[1])
+        self.input_At1 = self.Tensor(data1[2])
+
+        self.input_Bt0 = self.Tensor(data2[0])
+        self.action_B = self.Tensor(data2[1])
+        self.input_Bt1 = self.Tensor(data2[2])
+
+    def forward(self):
+        self.real_At0 = Variable(self.input_At0)
+        self.action_A = Variable(self.action_A)
+        self.real_At1 = Variable(self.input_At1)
+
+        self.real_Bt0 = Variable(self.input_Bt0)
+        self.action_B = Variable(self.action_B)
+        self.real_Bt1 = Variable(self.input_Bt1)
+
+    def test(self):
+        # forward
+        self.forward()
+        # G_A and G_B
+        self.backward_G()
+        self.backward_D_B()
+
+    def backward_D_basic(self, netD, real, fake):
+        # Real
+        pred_real = netD(real)
+        loss_D_real = self.criterionGAN(pred_real, True)
+        # Fake
+        pred_fake = netD(fake.detach())
+        loss_D_fake = self.criterionGAN(pred_fake, False)
+        # Combined loss
+        loss_D = (loss_D_real + loss_D_fake) * 0.5
+        # backward
+        if self.isTrain:
+            loss_D.backward()
+        return loss_D
+
+    def backward_D_A(self):
+        fake_A = self.fake_A_pool.query(self.fake_At0)
+        loss_D_1 = self.backward_D_basic(self.netD_1, self.real_At0, fake_A)
+        self.loss_D_1 = loss_D_1.item()
+
+    def backward_D_B(self):
+        fake_B = self.fake_B_pool.query(self.fake_Bt0)
+        loss_D_2 = self.backward_D_basic(self.netD_2, self.real_Bt0, fake_B)
+        self.loss_D_2 = loss_D_2.item()
+
+    def backward_G(self):
+        lambda_G_B0 = self.opt.lambda_G0
+        lambda_G_A0 = self.opt.lambda_G0
+        lambda_G_B1 = self.opt.lambda_G1
+        lambda_G_action_A = self.opt.lambda_GactA
+        lambda_G_action_B = self.opt.lambda_GactB
+        lambda_cycle_action = self.opt.lambda_Gcyc
+        lambda_F = self.opt.lambda_F
+
+        # ***************************
+        #       state cycle part
+        # ***************************
+
+        # generator target state -> source state loss
+        fake_At0 = self.netG_2to1(self.real_Bt0)
+        pred_fake = self.netD_1(fake_At0)
+        loss_G_Bt0 = self.criterionGAN(pred_fake, True) * lambda_G_B0
+
+        # generator source state -> target state loss
+        fake_Bt0 = self.netG_1to2(self.real_At0)
+        pred_fake = self.netD_2(fake_Bt0)
+        loss_G_At0 = self.criterionGAN(pred_fake, True) * lambda_G_A0
+
+        # cycle consistency part
+        cycle_fake_At0 = self.netG_1to2(fake_At0)
+        cycle_label_2 = torch.zeros_like(self.real_Bt0).float().cuda()
+
+        cycle_fake_Bt0 = self.netG_2to1(fake_Bt0)
+        cycle_label_1 = torch.zeros_like(self.real_At0).float().cuda()
+        loss_cycle = (self.criterionCycle(cycle_fake_At0 - self.real_Bt0, cycle_label_2) +
+                      self.criterionCycle(cycle_fake_Bt0 - self.real_At0, cycle_label_1)) * lambda_F
+
+        # ***************************
+        #       action part
+        # ***************************
+        with torch.no_grad():
+            fake_target_t0 = self.netG_1to2(self.real_At0)
+            fake_target_t1 = self.netG_1to2(self.real_At1)
+            target_action = self.netI_2(fake_target_t0, fake_target_t1)
+
+        pred_fake_target_action = self.net_action_G_1to2(self.real_At0, self.action_A)
+        label = torch.zeros_like(pred_fake_target_action).float().cuda()
+        loss_action = self.criterionCycle(target_action - pred_fake_target_action, label) * lambda_F
+
+        # # GAN loss D_B(G_B(B))
+        # fake_At1 = self.netF_A(fake_At0, fake_action_A)
+        # pred_fake = self.netD_B(fake_At1)
+        # loss_G_Bt1 = self.criterionGAN(pred_fake, True) * lambda_G_B1
+        #
+        # # dynamics cycle loss
+        # pred_At1 = self.netG_B(self.real_Bt1)
+        # cycle_label = torch.zeros_like(fake_At1).float().cuda()
+        # loss_cycle = self.criterionCycle(fake_At1 - pred_At1, cycle_label) * lambda_F
+
+        # ***************************
+        #       loss backward part
+        # ***************************
+
+        # combined loss
+        loss_G = loss_G_Bt0 + loss_G_At0 + loss_cycle
+        loss = loss_G + loss_action
+
+        if self.isTrain:
+            loss.backward()
+
+        # ***************************
+        #      postprocess part
+        # ***************************
+        # get the data for training discriminator
+
+        self.fake_At0 = fake_At0.data
+        self.fake_Bt0 = fake_Bt0.data
+
+        self.loss_G_B = loss_G_Bt0.item()
+        self.loss_G_A = loss_G_At0.item()
+        self.loss_cycle = loss_cycle.item()
+        self.loss_action_effect = loss_action.item()
+
+        self.loss_state_lt0, self.loss_state_lt1 = 0, 0
+
+        self.gt0 = self.real_Bt0
+        self.gt1 = self.real_Bt1
+        self.gt_buffer.append(self.gt0.cpu().data.numpy())
+        self.pred_buffer.append(self.fake_At0.cpu().data.numpy())
+
+    def optimize_parameters(self):
+        # forward
+        self.forward()
+        # G_A and G_B
+        self.optimizer_G.zero_grad()
+        self.backward_G()
+        self.optimizer_G.step()
+        # D_B
+        self.optimizer_D_2.zero_grad()
+        self.backward_D_B()
+        self.optimizer_D_2.step()
+        # D_A
+        self.optimizer_D_1.zero_grad()
+        self.backward_D_A()
+        self.optimizer_D_1.step()
+
+    def fetch(self):
+        real_B = (self.real_Bt0.cpu().data.numpy(),
+                  self.action_B.cpu().data.numpy(),
+                  self.real_Bt1.cpu().data.numpy())
+        fake_B = (self.fake_At0.cpu().data.numpy(),
+                  self.action_B.cpu().data.numpy(),
+                  self.fake_At1.cpu().data.numpy())
+        return (real_B, fake_B)
+
+    def get_current_errors(self):
+        ret_errors = OrderedDict([
+            ('G_B2A', self.loss_G_B),
+            ('G_A2B', self.loss_G_A),
+            ('cycle', self.loss_cycle),
+            ('action_effect', self.loss_action_effect),
+            ('D_A', self.loss_D_1),
+            ('D_B', self.loss_D_2)
+        ])
+        return ret_errors
+
+    # helper saving function that can be used by subclasses
+    def save_network(self, network, network_label, path):
+        save_filename = 'model_{}.pth'.format(network_label)
+        save_path = os.path.join(path, save_filename)
+        torch.save(network.state_dict(), save_path)
+
+    def save(self, path):
+        self.save_network(self.netG_B, 'G_B', path)
+        self.save_network(self.netD_B, 'D_B', path)
+        self.save_network(self.net_action_G_B, 'G_act_B', path)
+        self.save_network(self.net_action_G_A, 'G_act_A', path)
+        self.save_network(self.net_action_D_A, 'D_act_A', path)
+        self.save_network(self.net_action_D_B, 'D_act_B', path)
+
+    def load_network(self, network, network_label, path):
+        weight_filename = 'model_{}.pth'.format(network_label)
+        weight_path = os.path.join(path, weight_filename)
+        network.load_state_dict(torch.load(weight_path))
+
+    def load(self, path):
+        self.load_network(self.netG_B, 'G_B', path)
+        self.load_network(self.netD_B, 'D_B', path)
+        self.load_network(self.net_action_G_B, 'G_act_B', path)
+        self.load_network(self.net_action_G_A, 'G_act_A', path)
+        self.load_network(self.net_action_D_A, 'D_act_A', path)
+        self.load_network(self.net_action_D_B, 'D_act_B', path)
+
+    def reset_buffer(self):
+        self.gt_buffer = []
+        self.pred_buffer = []
+        self.gt_buffer1 = []
+        self.pred_buffer1 = []
+
+    def show_points(self, gt_data, pred_data):
+        ncols = int(np.sqrt(gt_data.shape[1])) + 1
+        nrows = int(np.sqrt(gt_data.shape[1])) + 1
+        assert (ncols * nrows >= gt_data.shape[1])
+        _, axes = plt.subplots(ncols, nrows, figsize=(nrows * 3, ncols * 3))
+        axes = axes.flatten()
+
+        for ax_i, ax in enumerate(axes):
+            if ax_i >= gt_data.shape[1]:
+                continue
+            ax.scatter(gt_data[:, ax_i], pred_data[:, ax_i], s=3, label='xyz_{}'.format(ax_i))
+
+    def npdata(self, item):
+        return item.cpu().data.numpy()
+
+    def visual(self, path):
+        gt_data = np.vstack(self.gt_buffer)
+        pred_data = np.vstack(self.pred_buffer)
+        # self.show_points(gt_data,pred_data)
+        # plt.savefig(path)
+        # plt.cla()
+        # plt.clf()
+
+        # gt_data = np.vstack(self.gt_buffer1)
+        # pred_data = np.vstack(self.pred_buffer1)
+        # print(list(abs(gt_data - pred_data).mean(0)), abs(gt_data - pred_data).mean())
+        self.reset_buffer()
