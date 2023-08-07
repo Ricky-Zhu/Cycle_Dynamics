93e5651659f268d25aadd98b09c92cb754144efa
Ruiqi Zhu <zhuruiqi1@gmail.com>
2023-08-07 10:56:30 +0100

    evaluation of baseline

--- cross_morphology/cycle_transfer/run_swimmer.sh
+++ cross_morphology/cycle_transfer/run_swimmer.sh
@@ -1,2 +1,2 @@
-python alignexp.py --env HalfCheetah-v2 --target_env HalfCheetah_3leg-v2 \
+python alignexp.py --env Swimmer-v2 --target_env Swimmer_4part-v2 \
 --pair_n 7000 --display_gap 1000 --eval_gap 1000 --eval_n 10 --pretrain_f True --seed $1
